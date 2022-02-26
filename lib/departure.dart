7c901c3353e88654e21209b694655cc813d48e29
Till Gerken <till.gerken@gmail.com>
2022-02-26 16:25:11 +0100

    Add departure class

---
+++ lib/departure.dart
@@ -0,0 +1,12 @@
+class Departure {
+  final String callsign;
+  final String destination;
+  final String sid;
+  final String squawk;
+
+  const Departure(
+      {required this.callsign,
+      required this.destination,
+      required this.sid,
+      required this.squawk});
+}
