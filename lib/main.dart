d1feaa94a235c3bbfc13ebc0aaa70805f9245285
Till Gerken <till.gerken@gmail.com>
2022-02-26 21:15:32 +0100

    First correct page build

--- lib/main.dart
+++ lib/main.dart
@@ -105,32 +105,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: BlocBuilder<DepartureListBloc, DepartureListState>(
-        builder: (context, departureListState) {
-          final departureList = departureListState as CurrentDepartureList;
+        builder: (context, depListState) {
+          final departureListState = depListState as CurrentDepartureList;
           return Center(
             // Center is a layout widget. It takes a single child and positions it
             // in the middle of the parent.
             child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Invoke "debug painting" (press "p" in the console, choose the
-              // "Toggle Debug Paint" action from the Flutter Inspector in Android
-              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-              // to see the wireframe for each widget.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Table(
                   border: TableBorder.all(),
                   defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                  children: departureList.departures
+                  children: departureListState.departures
                       .map((departure) => TableRow(
                             children: [
                               Text(departure.callsign),
@@ -157,7 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: () => _incrementCounter(context),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
