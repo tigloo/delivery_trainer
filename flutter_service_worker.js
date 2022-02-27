8ee4c0006a77383118b18e84d8a5a33de944b5e2
Till Gerken <till.gerken@gmail.com>
2022-02-27 13:05:21 +0100

    Built web
    Branch: main
    Commit: 4d95a0d4be5372869181273aee822e58170723a6
    
    package:peanut 4.1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "646a1cbc27603dbcba0017ce63d15e78",
-"index.html": "3e9b02075eb6461ddcf155ae05433a35",
-"/": "3e9b02075eb6461ddcf155ae05433a35",
+"index.html": "21e86229e88b62002804e9bf050e8e1e",
+"/": "21e86229e88b62002804e9bf050e8e1e",
 "main.dart.js": "d2d3e6b99c4b9e4839104bc8651aa5a5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -17,7 +17,11 @@ const RESOURCES = {
 "assets/NOTICES": "e5adae60b6558e4703dfef16b18279bd",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81"
+"assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
 };
 
 // The application shell files that are downloaded before a service worker can
